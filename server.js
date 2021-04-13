f98451adcb31f594e4a203e4b49291a20742f7a6
Ankush <ankushchalla@gmail.com>
2021-04-13 16:10:50 -0700

    Fixed dotenv config.

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  app.use(express.static("client/build"));
+}
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
